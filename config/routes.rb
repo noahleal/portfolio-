73b95b55f48f05f05d8cc39bd0729a25ba951120
Noah Leal <noahleal@me.com>
2019-07-08 23:00:15 +0200

    adds about page, fixes routes, updates gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "/", to: "homepage#home", as: "root"
+  get "/", to: "pages#home", as: "root"
   get "about", to: "pages#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
